83d65e0f3c4a0abbb866b6a22cedc472797d18aa
AJcodes04 <Ajfromla04@gmail.com>
2025-11-19 01:23:56 -0800

    Create Database header supporting university queries, comparisons, and GPA-based filtering

--- include/database.h
+++ include/database.h
@@ -6,7 +6,6 @@ Class: Database
 stores all the universities in a vector
 
 methods for:
-reading csv files and storing it in variables of classes
 looking up a university by name
 comparing data from other universities
 print summaries
@@ -15,3 +14,32 @@ other helper functions
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------
 */
+
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <string>
+#include <vector>
+#include "university.h"
+
+class Database
+{
+private:
+    std::vector<University> universities;
+    bool containsUniversity(const std::string &name) const;
+    bool validInput(const std::string &userInput) const;
+    void sortUniversitiesByGPA();
+
+public:
+    void printAllNames() const;
+    void printAllUniversityStats() const;
+    void compareTwoUniversities(const std::string &universityOne, const std::string &universityTwo) const;
+    void filterByMajorGPA(const std::string &major, double GPA) const;
+
+    // return type, the university object
+    const University *getUniversity(const std::string &universityName) const;
+    std::vector<std::string> getAllUniversityNames() const;
+    const std::vector<University> &getAllUniversities() const;
+};
+
+#endif
