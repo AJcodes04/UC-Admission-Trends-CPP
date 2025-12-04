d27fa9e0ec13449fd29401de2c6b07f3e70043cb
VincentNguyenCodes <vincentnguyen0535@gmail.com>
2025-12-04 00:51:07 -0800

    implemented database.cpp and university.cpp

--- src/database.cpp
+++ src/database.cpp
@@ -6,11 +6,33 @@
 #include <vector>
 
 void Database::addSchool(const std::string& name) {
+    for(int i = 0; i<schools.size(); i++){
+        School* s = schools[i];
+        if(s->name == name){
+            return;
+        }
+    }
+    School *s = new School(name);
+    schools.push_back(s);
 }
 
 School* Database::getSchool(const std::string& name) {
+    for(int i = 0; i<schools.size(); i++){
+        School* s = schools[i];
+        if(s->name==name)
+            return(s);
+    }
+
     return nullptr;
 }
 
 void Database::insertMajor(const std::string& schoolName, int year, Major* major) {
+    School* s = getSchool(schoolName);
+
+    if(!s){
+        addSchool(schoolName);
+        s = getSchool(schoolName);
+    }
+
+    s->years->addMajorToYear(year, major);
 }
--- src/university.cpp
+++ src/university.cpp
@@ -4,30 +4,75 @@
 #include <vector>
 #include <string>
 
-// -------------------
-// YearList Management
-// -------------------
+YearList::YearList(){
+    head = nullptr;
+}
 
-YearList::YearList()
-    : head(nullptr) {}
-
-// Free the linked list
 YearList::~YearList() {
+    freeList();
 }
 
 void YearList::freeList() {
+    YearNode* curr = head;
+    
+    while(curr != nullptr){
+        for(int i=0; i<curr->majors.size(); i++)
+            delete curr->majors[i];
+
+    YearNode* nextNode = curr->next;
+    delete curr;
+    curr = nextNode;
+    }
+
+    head = nullptr;
+
 }
 
 void YearList::addYear(int year) {
+    if(head == nullptr){
+        head == new YearNode(year);
+        return;
+    }
+
+    YearNode* curr = head;
+
+    while(curr->next != nullptr){
+        if(curr->year == year)
+            return;
+        curr = curr->next;
+    }
+
+    if(curr->next->year == year)
+        return;
+
+    curr->next = new YearNode(year);
 }
 
 YearNode* YearList::findYear(int year) {
+    YearNode* curr = head;
+
+    while(curr != nullptr){
+        if(curr->year == year)
+            return curr;
+        curr = curr->next;
+    }
+
     return nullptr;
 }
 
 void YearList::addMajorToYear(int year, Major* major) {
-}
+    YearNode* node = findYear(year);
 
+    if (node == nullptr) {
+        addYear(year);
+        node = findYear(year);  // now it must exist
+    }
 
-School::~School() {
+    for(int i=0; i<node->majors.size(); i++){
+        if(node->majors[i]->majorName == major->majorName){
+            return;
+        }
+    }
+
+    node->majors.push_back(major);
 }
